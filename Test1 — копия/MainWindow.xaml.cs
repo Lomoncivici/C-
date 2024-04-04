087a59d5122a40becd05931fa238e78ff538a62f
Lomoncivici <125694971+Lomoncivici@users.noreply.github.com>
2024-04-04 13:39:44 +0300

    C# 4practoz

---
+++ Test1 — копия/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Test1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PassTest_Click(object sender, RoutedEventArgs e)
+        {
+            var authWindow = new Test2(null,0,false);
+            authWindow.Show();
+            Close();
+        }
+
+        private void EditTest_Click(object sender, RoutedEventArgs e)
+        {
+            switch (TextPassword.Visibility)
+            {
+                case Visibility.Visible:
+                    if (TextPassword.Text == "12345")
+                    {
+                        TextPassword.BorderBrush = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FFABADB3"));
+                        var authWindow = new Redactor();
+                        authWindow.Show();
+                        Close();
+                    }
+                    else
+                    {
+                        TextPassword.BorderBrush = new SolidColorBrush(Colors.Red);
+                    }
+                    break;
+
+                case Visibility.Hidden:
+                    TextPassword.Visibility = Visibility.Visible;
+                    break;
+            }
+        }
+    }
+}
---
+++ Test1 — копия/Test2.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Test1
+{
+    public partial class Test2 : Window
+    {
+        public Test2(object a, int ball, bool gg)
+        {
+            InitializeComponent();
+
+            if (gg == true)
+            {
+                Ball.Text = ball.ToString();
+                Ball.Visibility = Visibility.Visible;
+            }
+
+            switch (a)
+            {
+                default:
+                    PageFrame.Content = new Page1();
+                    break;
+                case 2:
+                    PageFrame.Content = new Page2();
+                    break;
+                case 3:
+                    PageFrame.Content = new Page3();
+                    break;
+            }
+        }
+
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            var authWindow = new MainWindow();
+            authWindow.Show();
+            Close();
+        }
+    }
+}
