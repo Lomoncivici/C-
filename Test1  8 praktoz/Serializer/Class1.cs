4187399e48b21be14e595c4c9013628106cf3128
Lomoncivici <125694971+Lomoncivici@users.noreply.github.com>
2024-05-26 17:13:30 +0300

    Add files via upload

---
+++ Test1  8 praktoz/Serializer/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SerializationLibrary
+{
+    public static class Serializer
+    {
+        public static async Task SerializeAsync<T>(T obj, string filePath)
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            using (FileStream createStream = File.Create(filePath))
+            {
+                await JsonSerializer.SerializeAsync(createStream, obj, options);
+            }
+        }
+
+        public static async Task<T> DeserializeAsync<T>(string filePath)
+        {
+            using (FileStream openStream = File.OpenRead(filePath))
+            {
+                return await JsonSerializer.DeserializeAsync<T>(openStream);
+            }
+        }
+    }
+}
